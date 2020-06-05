2dec816c6b622bc7c845ef2609b6e21f704b4bb9
DrKLO <drklo.2kb@gmail.com>
2020-06-05 17:08:29 +0300

    1986

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -18,8 +18,8 @@ public class BuildVars {
     public static boolean LOGS_ENABLED = false;
     public static boolean USE_CLOUD_STRINGS = true;
     public static boolean CHECK_UPDATES = false;
-    public static boolean TON_WALLET_STANDALONE = false;=
-    public static int BUILD_VERSION = 1985;
+    public static boolean TON_WALLET_STANDALONE = false;
+    public static int BUILD_VERSION = 1986;
     public static String BUILD_VERSION_STRING = "6.2.0";
     public static int APP_ID = 324507;
     public static String APP_HASH = "8b18e01437686393a6ab21d1d58fd3ae";
