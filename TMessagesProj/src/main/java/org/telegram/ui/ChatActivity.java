60983fc626e3336f23013864b52ea86a37b3c976
DrKLO <drklo.dev@gmail.com>
2020-11-01 00:13:37 +0300

    Merge 7.2.1 / 2139

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -503,6 +503,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
 
     private boolean openAnimationEnded;
     private boolean fragmentOpened;
+    private boolean fragmentBeginToShow;
     private long openAnimationStartTime;
 
     private boolean scrollToTopOnResume;
@@ -647,6 +648,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
     private long activityResumeTime;
 
     private int transitionAnimationIndex;
+    private int scrollAnimationIndex;
     private final static int[] allowedNotificationsDuringChatListAnimations = new int[]{
             NotificationCenter.messagesRead,
             NotificationCenter.threadMessagesRead,
@@ -1514,6 +1516,8 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
         if (chatAttachAlert != null) {
             chatAttachAlert.dismissInternal();
         }
+        getNotificationCenter().onAnimationFinish(transitionAnimationIndex);
+        getNotificationCenter().onAnimationFinish(scrollAnimationIndex);
         hideUndoViews();
         if (chatInviteRunnable != null) {
             AndroidUtilities.cancelRunOnUIThread(chatInviteRunnable);
@@ -4071,13 +4075,12 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
         if (MessagesController.getGlobalMainSettings().getBoolean("view_animations", true)) {
             chatListItemAniamtor = new ChatListItemAnimator(this, chatListView) {
 
-                int index = -1;
                 Runnable finishRunnable;
 
                 @Override
                 public void onAnimationStart() {
-                    if (index == -1) {
-                        index = getNotificationCenter().setAnimationInProgress(index, allowedNotificationsDuringChatListAnimations, false);
+                    if (scrollAnimationIndex == -1) {
+                        scrollAnimationIndex = getNotificationCenter().setAnimationInProgress(scrollAnimationIndex, allowedNotificationsDuringChatListAnimations, false);
                     }
                     if (finishRunnable != null) {
                         AndroidUtilities.cancelRunOnUIThread(finishRunnable);
@@ -4099,9 +4102,9 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                             scrollToMessageId(nextScrollToMessageId, nextScrollFromMessageId, nextScrollSelect, nextScrollLoadIndex, nextScrollForce);
                             nextScrollToMessageId = 0;
                         }
-                        if (index != -1) {
-                            getNotificationCenter().onAnimationFinish(index);
-                            index = -1;
+                        if (scrollAnimationIndex != -1) {
+                            getNotificationCenter().onAnimationFinish(scrollAnimationIndex);
+                            scrollAnimationIndex = -1;
                         }
                         if (BuildVars.LOGS_ENABLED) {
                             FileLog.d("chatItemAnimator enable notifications");
@@ -4117,9 +4120,9 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                         AndroidUtilities.cancelRunOnUIThread(finishRunnable);
                     }
                     AndroidUtilities.runOnUIThread(finishRunnable = () -> {
-                        if (index != -1) {
-                            getNotificationCenter().onAnimationFinish(index);
-                            index = -1;
+                        if (scrollAnimationIndex != -1) {
+                            getNotificationCenter().onAnimationFinish(scrollAnimationIndex);
+                            scrollAnimationIndex = -1;
                         }
                         if (BuildVars.LOGS_ENABLED) {
                             FileLog.d("chatItemAnimator enable notifications");
@@ -11295,11 +11298,12 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                 } else {
                     doNotRemoveLoadIndex = false;
                 }
-                if (!doNotRemoveLoadIndex && !openAnimationEnded) {
+                if (!doNotRemoveLoadIndex && !fragmentBeginToShow && !paused) {
                     int[] alowedNotifications = new int[]{NotificationCenter.chatInfoDidLoad, NotificationCenter.dialogsNeedReload, NotificationCenter.scheduledMessagesUpdated,
                             NotificationCenter.closeChats, NotificationCenter.botKeyboardDidLoad, NotificationCenter.userInfoDidLoad, NotificationCenter.pinnedInfoDidLoad, NotificationCenter.needDeleteDialog/*, NotificationCenter.botInfoDidLoad*/};
                     if (transitionAnimationIndex == 0) {
                         transitionAnimationIndex = getNotificationCenter().setAnimationInProgress(transitionAnimationIndex, alowedNotifications);
+                        AndroidUtilities.runOnUIThread(() -> getNotificationCenter().onAnimationFinish(transitionAnimationIndex), 800);
                     } else {
                         getNotificationCenter().updateAllowedNotifications(transitionAnimationIndex, alowedNotifications);
                     }
@@ -11508,9 +11512,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                     firstLoading = false;
                     AndroidUtilities.runOnUIThread(() -> {
                         getNotificationCenter().runDelayedNotifications();
-                        if (parentLayout != null) {
-                            parentLayout.resumeDelayedFragmentAnimation();
-                        }
+                        resumeDelayedFragmentAnimation();
                     });
                 }
 
@@ -15082,6 +15084,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
     public void onTransitionAnimationStart(boolean isOpen, boolean backward) {
         int[] alowedNotifications = null;
         if (isOpen) {
+            fragmentBeginToShow = true;
             if (transitionAnimationIndex == 0) {
                 alowedNotifications = new int[]{
                         NotificationCenter.dialogsNeedReload, NotificationCenter.closeChats,
